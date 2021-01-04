2bd493c6c5531caaa0371472fdf7571ac1ce8c9d
Matheus Ferreira <matheusguiferreira@hotmail.com>
2021-01-04 04:38:34 -0300

    Added character tiers

---
+++ src/main/java/enums/Tiers.java
@@ -0,0 +1,12 @@
+package enums;
+
+/**
+ * enums
+ *
+ * @author wyvern800 - http://github.com/wyvern800
+ * @created 04/01/2021 - 04:24
+ * @project demo
+ */
+public enum Tiers {
+    S, A, B, C, D
+}
